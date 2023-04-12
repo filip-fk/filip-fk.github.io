b00a50b361cda4b4bde1e6a541ea5d83fb5aa090
filip-fk <filipkalapish@outlook.com>
2023-04-12 16:41:39 +0200

    new design - incomplete 3

---
+++ js/slides.js
@@ -0,0 +1,18 @@
+/*let swiper = new Swiper(".mySwiper", {
+    lazy: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});*/
+
+let swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
